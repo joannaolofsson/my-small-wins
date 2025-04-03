6073b7d66a38717566593c66df3b012ec2573983
Joanna Olofsson <joanna.olofsson@gmail.com>
2025-04-03 17:44:45 +0200

    install most components

--- src/app/futureself/page.tsx
+++ src/app/futureself/page.tsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import { Card, CardTitle, CardContent } from '@/components/ui/card'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 
 function Futureself() {
   return (
-    <div>Futureself</div>
+    <div className="flex w-full justify-center m-8"> {/* Här skall de kort som lagts till i imput fälten visas med en länk att klicka vidare...*/}
+       <div className="flex flex-col items-start gap-8 ">
+        <Button>Tillbaka</Button>
+        <Card className='flex flex-col text-lg'>
+          <Link href="#">
+          <CardTitle className='px-6 py-4'>
+            Card 1
+          </CardTitle>
+          </Link>
+        </Card>
+        <Card className='flex flex-col text-lg'>
+          <Link href="#">
+          <CardTitle className='px-6 py-4'>
+            Card 1
+          </CardTitle>
+          </Link>
+        </Card>
+        <Card className='flex flex-col text-lg'>
+          <Link href="#">
+          <CardTitle className='px-6 py-4'>
+            Card 1
+          </CardTitle>
+          </Link>
+        </Card>
+      </div>
+    </div>
   )
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
-import Image from "next/image";
+
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <h1>Home</h1>
+    <div className="flex w-full justify-center m-8">
+      <div className="flex flex-col items-start gap-4">
+        <h1 className="text-4xl tex">Small win</h1>
+        <h2 className="text-2xl">tracker</h2>
+        <p>What habits does your future self have?</p>
+        <p>Track your small wins today and meet a future you want to live in.</p>
+        <div>
+          <Button>Start</Button>
+        </div>
+      </div>
+    </div>
   );
 }
